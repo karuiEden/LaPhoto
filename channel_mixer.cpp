8e4a35280ef5dee7ec065ee50dba5cbf8496e08d
karui <starikov.mak2002@gmail.com>
2025-08-01 15:03:53 +0300

    Channel Mixer + Example

--- channel_mixer.cpp
+++ channel_mixer.cpp
@@ -1,17 +1,40 @@
 //
 // Created by karui on 7/28/2025.
 //
+#include "channel_mixer.h"
+
 #include <opencv2/opencv.hpp>
 #include <vector>
 #include <cstdint>
 using namespace std;
-
-optional<cv::Mat> channel_mixer(const cv::Mat& input, const vector<int32_t>& red_channel, const vector<int32_t>& green_channel, const vector<int32_t>& blue_channel, const vector<int32_t>& constants) {
+//Make mixer of channels. If input is empty, function returns nullopt.
+optional<cv::Mat> channel_mixer(const cv::Mat& input, const ChannelMix& r_mixer, const ChannelMix& g_mixer, const ChannelMix& b_mixer, const ChannelMix& gray_mixer, bool monochrome) {
     if (input.empty()) {
         return nullopt;
     }
-    const uint32_t height = input.cols;
-    const uint32_t weight = input.rows;
-
-
+    vector<cv::Mat> channels;
+    cv::split(input, channels);
+    cv::Mat R, G, B;
+    channels[0].convertTo(B, CV_32F);
+    channels[1].convertTo(G, CV_32F);
+    channels[2].convertTo(R, CV_32F);
+    cv::Mat output;
+    if (monochrome) {
+        cv::Mat gray = R * gray_mixer.red + G * gray_mixer.green + B * gray_mixer.blue + gray_mixer.constant;
+        gray = cv::min(cv::max(0, gray), 255);
+        gray.convertTo(output, CV_8U);
+        cv::cvtColor(gray, output, cv::COLOR_GRAY2BGRA);
+    } else {
+        cv::Mat outputRed = R * r_mixer.red + G * r_mixer.green + B * r_mixer.blue + r_mixer.constant;
+        cv::Mat outputGreen = R * g_mixer.red + G * g_mixer.green + B * g_mixer.blue + g_mixer.constant;
+        cv::Mat outputBlue = R * b_mixer.red + G * b_mixer.green + B * b_mixer.blue + b_mixer.constant;
+        cv::Mat UB, UG, UR;
+        outputBlue.convertTo(UB, CV_8U);
+        outputGreen.convertTo(UG, CV_8U);
+        outputRed.convertTo(UR, CV_8U);
+        vector outputBGR = {UB, UG, UR};
+        cv::merge(outputBGR, output);
+        cv::cvtColor(output, output, cv::COLOR_BGR2BGRA);
+    }
+    return output;
 }
--- main.cpp
+++ main.cpp
@@ -2,45 +2,46 @@
 #include <vector>
 
 #include "algorithm.h"
+#include "channel_mixer.h"
 #include "convert.h"
 using namespace std;
 
 
 int main(const int32_t argc, char** argv) {
-    if (argc < 3) {
+    if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <image_path>" << "<output_path>" << std::endl;
         return -1;
     }
     const string image_path = argv[1];
-    const string output_path = argv[2];
+    const string output_path = argc == 3 ? argv[2] : "../output/";
     cv::Mat image = cv::imread(image_path, cv::IMREAD_UNCHANGED); // Загрузка с альфой, если есть
-
     if (image.empty()) {
         std::cerr << "Could not open or find the image." << std::endl;
         return -1;
     }
-
-    // Убедимся, что изображение в 4-канальном BGRA формате (CV_8UC4)
-    if (image.channels() == 3) { // Если RGB, добавить альфу
-        cv::cvtColor(image, image, cv::COLOR_BGR2BGRA);
-    } else if (image.channels() == 1) { // Если Grayscale, конвертировать в BGRA
-        cv::cvtColor(image, image, cv::COLOR_GRAY2BGRA);
+    ChannelMix b = {1.0, 1.0, 1.0, 1.0};
+    ChannelMix g = {1.0, 1.0, 1.0, 1.0};
+    ChannelMix r = {1.0, 1.0, 1.0, 1.0};
+    ChannelMix gray = {-0.7, 2.0, -0.3, 0.0};
+    auto mix_image_opt = channel_mixer(image, r, g, b, gray, true);
+    image.release();
+    if (!mix_image_opt) {
+        std::cerr << "Couldn't mixer of channel." << std::endl;
+        return -1;
     }
-    // Если изображение уже BGRA (или RGBA) с 8 бит на канал, то все хорошо.
-    // Если оно в другом формате (например, RGBA), нужно конвертировать.
-    if (image.channels() == 4 && image.type() != CV_8UC4) {
-        // Конвертация в CV_8UC4, если нужно (например, если было CV_16UC4)
-        image.convertTo(image, CV_8UC4);
-    }
+    auto& mix_image = mix_image_opt.value();
+    mix_image.convertTo(mix_image, CV_8UC4);
+    cv::imwrite("../output/rose_gray.JPG", mix_image);
+    // mix_image.release();
+    const int width = mix_image.cols;
+    const int height = mix_image.rows;
 
-    const int width = image.cols;
-    const int height = image.rows;
-
-    const auto src_pixels_argb_opt = mat_to_argb_vector(image);
+    const auto src_pixels_argb_opt = mat_to_argb_vector(mix_image);
     if (!src_pixels_argb_opt) {
         std::cerr << "Could not convert image to ARGB." << std::endl;
         return -1;
     }
+
     const auto& src_pixels_argb = src_pixels_argb_opt.value();
     std::vector<int32_t> dst_pixels_argb(width * height);
 
@@ -57,7 +58,6 @@ int main(const int32_t argc, char** argv) {
     // Конвертация обратно в cv::Mat для сохранения
     cv::Mat output_image = argb_vector_to_mat(dst_pixels_argb, width, height);
     cv::imwrite(output_path, output_image); // Сохранение в PNG (поддерживает альфу)
-    image.release();
     output_image.release();
     return 0;
 }
